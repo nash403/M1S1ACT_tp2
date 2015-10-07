ad8e8d49ed4d066a4b8577fd1112f7f84a387e21
nash403 <honore.nintunze@gmail.com>
2015-10-07 13:10:10 +0200

    commentaires finis, reste réponses aux questions

--- skyline.hs
+++ skyline.hs
@@ -26,19 +26,29 @@ realMS:: Int -> Int -> Skyline -> Skyline -> Skyline
 realMS _ _ xs []                   = xs
 realMS _ _ [] xs                   = xs
 -- les deux skylines ne sont pas vides
+-- (m,n) est le point courrant dans s1, (x,y) est le point courrant dans s2
+-- dans les commentaires qui suivent (m-1,n-1) (resp. (x-1,y-1)) désigne le dernier point visité dans s1 (resp. s2)
 realMS h1 h2 ((m,n):ms) ((x,y):xs)
-        -- si le point courrant de s1 est après le point courrant de s2 
-        | m > x     = case h1 >= y of True  -> ( -- le dernier point visité de s1 est plus haut que le point courrant de s2
-                              -- le dernier point visité de s2 est plus haut que le denier point visité de s1 et donc du point courrant de s1
+        -- si (m,n) est après (x,y) suivant l'axe des abscisses
+        | m > x     = case h1 >= y of True  -> ( -- (m-1,n-1) est plus haut que (x,y), (x,y) est donc sous la ligne de toit et n'est pas visible
+                              -- le dernier point visité de s2 est plus haut que le dernier point visité de s1
                                         if (h2 >= h1)
                                         then
+                              -- si c'est la même hauteur on consomme (x,y) sans l'ajouter et on fusionne le reste
+                              -- sinon il faut ajouter une intersection au dessus de (x,y), on consomme (x,y) en ajoutant l'intersection et on fusionne le reste
                                           (if h2 == h1 then realMS h1 y ((m,n):ms) xs else (x,h1):realMS h1 y ((m,n):ms) xs)
                                         else
+                              -- si (x,y) est en dessous de (m-1,n-1) et que (m,n) est au dessus ou à la même hauteur que (x,y) alors (x,y) n'est pas visible, on consomme (x,y) sans l'ajouter et on fusionne le reste
+                              -- sinon il faut ajouter une intersection au dessus de (m,n), on consomme (x,y) en ajoutant l'intersection et on fusionne le reste
                                           (if (n > y) || (h1 == y) then realMS h1 y ((m,n):ms) xs else (m,y):realMS h1 y ((m,n):ms) xs)
-                                        ) 
+                                        )
+                              -- (x,y) est au dessus de (m-1,n-1) donc visible, on consomme (x,y) en l'ajoutant et on fusionne le reste
                                       False -> (x,y):realMS h1 y ((m,n):ms) xs
-        | m < x     = case h2 > n of  True  -> realMS n h2 ms ((x,y):xs)
-                                      False -> (m,n):realMS n h2 ms ((x,y):xs)
+        -- si (m,n) est avant (x,y)
+        | m < x     = case h2 > n of  True  -> realMS n h2 ms ((x,y):xs) -- si (x-1,y-1) est au dessu de (m,n), (m,n) est caché on le consomme sans l'ajouter et on fusionne le reste
+                                      False -> (m,n):realMS n h2 ms ((x,y):xs) -- si (x-1,y-1) est en dessous de (m,n), (m,n) est au dessus de la skyline et donc visible, on consomme (m,n) en l'ajoutant et on fusionne le reste
+        -- si (m,n) est sur la même vericale que (x,y) on consomme (m,n) en l'ajoutant et (x,y) sans l'ajouter et on fusionne le reste
+        -- sinon c'est (x,y) qu'on consomme en l'ajoutant et (m,n) sans l'ajouter et on fusionne le reste 
         | otherwise = case n > y of True  -> (m,n):realMS n y ms xs
                                     False -> (x,y):realMS n y ms xs
 
