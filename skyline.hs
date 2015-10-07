a855800e098b51e4ee6b8ee4f4b953a4a054c8a9
nash403 <honore.nintunze@gmail.com>
2015-10-07 10:00:37 +0200

    premier jet de commentaires

--- skyline.hs
+++ skyline.hs
@@ -10,11 +10,11 @@ type Immeuble = (Int, Int, Int)
 type Point = (Int, Int)
 type Skyline = [Point]
 
--- Transforme un immeuble en Skyline composées de deux points 
+{- Transforme un immeuble en Skyline composées de deux points -}
 toSkyline::Immeuble -> Skyline
 toSkyline (g,h,d) = [(g,h),(d,0)]
 
--- Fonction intermédiaire appelée par le mergeSkyline et qui fait effectivement le merge
+{- Fonction intermédiaire appelée par mergeSkyline et qui fait effectivement le merge -}
 realMS:: Int -> Int -> Skyline -> Skyline -> Skyline
 realMS _ _ xs []                   = xs
 realMS _ _ [] xs                   = xs
@@ -32,37 +32,52 @@ realMS h1 h2 ((m,n):ms) ((x,y):xs)
         | otherwise = case n > y of True  -> (m,n):realMS n y ms xs
                                     False -> (x,y):realMS n y ms xs
 
--- On insère s1 dans s2
+{-
+  mergeSkyline s1 s2: Fusion de la skyline s1 dans s2
+  s1,s2: une skyline
+  Sortie : une skyline qui est la fusion de s1 et s2
+-}
 mergeSkyline::Skyline -> Skyline -> Skyline
 mergeSkyline s1 s2 = realMS 0 0 s1 s2
 
+{-
+  insereImmeuble i s: Insertion de l'immeuble i dans la skyline s
+  i un immeuble
+  s une skyline
+  Sortie : une skyline prenant en compte l'immeuble inséré
+-}
 insereImmeuble::Immeuble -> Skyline -> Skyline
 insereImmeuble i [] = toSkyline i
 insereImmeuble i s  = mergeSkyline (toSkyline i) s
 
+{-
+  split l: réparti les éléments d'une liste dans 2 listes de taille égale (à 1 élément près)
+  l une liste
+  Sortie : un couple avec les 2 sous listes
+-}
 split :: [a] -> ([a],[a])   
 split (x:y:zs) = (x:xs,y:ys) where (xs,ys) = split zs
 split xs       = (xs,[])
 
+{-
+  constructSkyline is: construit une skyline à partir d'une liste d'immeubles
+  is une liste d'immeubles
+  Sortie : une skyline comprenant tous les immeubles de is 
+-}
 constructSkyline::[Immeuble] -> Skyline
 constructSkyline [] = []
 constructSkyline [x] = toSkyline x
 constructSkyline xs = mergeSkyline (constructSkyline xs1) (constructSkyline xs2)
         where (xs1,xs2) = split xs
-{-
---Pour les test, transforme les immeubles en points de manière violente
-immeublesToSkyline:: [Immeuble] -> Skyline
-immeublesToSkyline []           = []
-immeublesToSkyline ((g,h,d):xs) = (g,h):(d,0):(immeublesToSkyline xs)-}
 
---Transforme une Skyline où un point sur deux est écrit en Skyline où tout les points sont écrit
+{- Transforme une Skyline où un point sur deux est écrit en Skyline où tout les points sont écrit -}
 listePointComplet:: Skyline -> Skyline
 listePointComplet xs                   = listePointCompletInter ((0,0):xs)
                                          where listePointCompletInter []                   = []
                                                listePointCompletInter (x:[])               = [x]
                                                listePointCompletInter ((x1,y1):(x2,y2):xs) = (x1,y1):(x2,y1):(listePointCompletInter ((x2,y2):xs))
 
---Ecris le SVG à partir de la skyline dans le fichier désigner par fileName
+{- Ecris le SVG à partir de la skyline dans le fichier désigner par fileName -}
 lineToSVG:: Skyline -> String -> IO ()
 lineToSVG skyLine fileName = do
                                 appendFile fileName "<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"300\" height=\"300\" viewBox=\"-10 -150 200 150\">\n<polyline points=\""
@@ -73,21 +88,21 @@ lineToSVG skyLine fileName = do
                                                                        appendFile fileName ((show x) ++ "," ++ (show y) ++ " ")
                                                                        toSVGPoint xs fileName
 
---Récupère le chemin du fichier sans l'extension
+{- Récupère le chemin du fichier sans l'extension -}
 suffFich     :: String -> String
 suffFich s =  case dropWhile (=='.') s of
                       "" -> ""
                       s' -> w 
                             where (w, s'') = break (=='.') s'
 
---Lis les immeubles à partir du fichier si celui si est bien fait
+{- Lis les immeubles à partir du fichier si celui si est bien fait -}
 readImmeuble:: [String] -> Int -> [Immeuble]
 readImmeuble _ 0    = []
 readImmeuble (l:ls) n = (toImmeuble listIm):(readImmeuble ls (n-1))
                         where toImmeuble [g,h,d] = ((read g::Int), (read h::Int), (read d::Int))
                               listIm = words l
 
---Pour executer le main lui passer en argument le lien du fichier contenant les immeubles
+{- Pour executer le main lui passer en argument le lien du fichier contenant les immeubles -}
 main = do
         (fichier:args) <- getArgs
         handle <- openFile fichier ReadMode
